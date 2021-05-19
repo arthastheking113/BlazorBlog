421e18a0b84ce2498ed07113817e1ed9dc00c510
arthas <arthastheking113@gmail.com>
2021-05-18 20:38:10 -0500

    update preloader

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,6 @@
 ﻿(function ($) {
     "use strict";
 
-    var nav = $('nav');
-    var navHeight = nav.outerHeight();
-
     // Preloader
     $(window).on('load', function () {
         if ($('#preloader').length) {
@@ -13,42 +10,6 @@
         }
     });
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-    /*--/ Star ScrollTop /--*/
-    $('.scrolltop-mf').on("click", function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    });
-
-
-
-    // Scroll to sections on load with hash links
-    if (window.location.hash) {
-        var initial_nav = window.location.hash;
-        if ($(initial_nav).length) {
-            var scrollto_initial = $(initial_nav).offset().top - mainNav_height;
-            $('html, body').animate({
-                scrollTop: scrollto_initial
-            }, 1000, "easeInOutExpo");
-        }
-    }
-
-    
 
    
 
