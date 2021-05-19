a06f166ef6c1c572efcb87d0f357b1b82589bd3a
arthas <arthastheking113@gmail.com>
2021-05-18 20:05:09 -0500

    update preloader

---
+++ wwwroot/js/site.js
@@ -0,0 +1,55 @@
+﻿(function ($) {
+    "use strict";
+
+    var nav = $('nav');
+    var navHeight = nav.outerHeight();
+
+    // Preloader
+    $(window).on('load', function () {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function () {
+                $(this).remove();
+            });
+        }
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    /*--/ Star ScrollTop /--*/
+    $('.scrolltop-mf').on("click", function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
+
+    // Scroll to sections on load with hash links
+    if (window.location.hash) {
+        var initial_nav = window.location.hash;
+        if ($(initial_nav).length) {
+            var scrollto_initial = $(initial_nav).offset().top - mainNav_height;
+            $('html, body').animate({
+                scrollTop: scrollto_initial
+            }, 1000, "easeInOutExpo");
+        }
+    }
+
+    
+
+   
+
+})(jQuery);
