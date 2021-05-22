a6cee88ec98bc7798bb8650a1b5cf16e7749c714
arthas <arthastheking113@gmail.com>
2021-05-22 02:40:51 -0500

    update upload avatar image

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorServer.Data;
 using BlazorServer.Model;
+using BlazorServer.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,13 +17,19 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<CustomUser> _userManager;
         private readonly SignInManager<CustomUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
 
         public IndexModel(
             UserManager<CustomUser> userManager,
-            SignInManager<CustomUser> signInManager)
+            SignInManager<CustomUser> signInManager,
+            ApplicationDbContext context,
+            IImageService imageService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
+            _imageService = imageService;
         }
 
         public string Username { get; set; }
@@ -36,6 +45,20 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+
+            [Display(Name = "First Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+
+            [Display(Name = "Last Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string LastName { get; set; }
+
+            [Display(Name = "Change Avatar")]
+            public Byte[]? ImageData { get; set; }
+            public string ContentType { get; set; }
         }
 
         private async Task LoadAsync(CustomUser user)
@@ -47,7 +70,11 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = user.PhoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                ImageData = user.ImageData,
+                ContentType = user.ContentType
             };
         }
 
@@ -63,14 +90,15 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(IFormFile image, Byte[]? imageData, string contentType)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
@@ -78,9 +106,29 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (image != null)
+            {
+                user.ImageData = await _imageService.EncodeFileAsync(image);
+                user.ContentType = _imageService.RecordContentType(image);
+            }
+            else
+            {
+                if (imageData != null && contentType != null)
+                {
+                    user.ImageData = imageData;
+                    user.ContentType = contentType;
+                }
+                else
+                {
+                    user.ImageData = null;
+                    user.ContentType = null;
+                }
+
+            }
             if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+
                 if (!setPhoneResult.Succeeded)
                 {
                     StatusMessage = "Unexpected error when trying to set phone number.";
@@ -88,6 +136,7 @@ namespace BlazorServer.Areas.Identity.Pages.Account.Manage
                 }
             }
 
+            await _context.SaveChangesAsync();
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
